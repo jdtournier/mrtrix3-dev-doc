a43bb0f3789643611d8f9c2b8e9d711d9be828dd
J-Donald Tournier <jdtournier@gmail.com>
2016-05-16 17:40:41 +0100

    automatic update of dev doc

--- fetch__store_8h.js
+++ fetch__store_8h.js
@@ -3,7 +3,7 @@ var fetch__store_8h =
     [ "__DEFINE_FETCH_STORE_FUNCTION_FOR_TYPE", "fetch__store_8h.html#a85b55a135b4b18ef1f54ac49c3b0179c", null ],
     [ "__DEFINE_FETCH_STORE_FUNCTIONS", "fetch__store_8h.html#a8ba288409645183aac38fec3d1f93b20", null ],
     [ "MRTRIX_EXTERN", "fetch__store_8h.html#a2ac0e3ee61ca417be1935bf4a3066011", null ],
-    [ "__set_fetch_store_functions", "fetch__store_8h.html#a00f164966747652cad1edde6849ef9af", null ],
+    [ "__set_fetch_store_functions", "fetch__store_8h.html#aed7282d40ce4ab6c26230922b7ab856e", null ],
     [ "__set_fetch_store_functions", "fetch__store_8h.html#a6808b73176fb9320cb23dfe948958620", null ],
     [ "__set_fetch_store_functions< bool >", "fetch__store_8h.html#a362bed6e0145cb428cd98934f82ba6a2", null ],
     [ "__set_fetch_store_functions< cdouble >", "fetch__store_8h.html#af0df2da37e7a16eee1d9ff626185333c", null ],
